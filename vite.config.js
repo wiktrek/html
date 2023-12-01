9cb8e129a74ed1c90d7663f27afac6b551beffd3
wiktrek <wiktor.janiszewski10@gmail.com>
2023-12-01 23:19:51 +0100

    body tag

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,19 @@
 import { resolve } from 'path';
+const paths = {
+  main: resolve(__dirname, 'index.html'),
+  a: resolve(__dirname, 'tag/a.html'),
+  p: resolve(__dirname, 'tag/p.html'),
+  div: resolve(__dirname, 'tag/div.html'),
+  marquee: resolve(__dirname, 'tag/marquee.html'),
+  html: resolve(__dirname, 'tag/html.html'),
+  head: resolve(__dirname, 'tag/head.html'),
+  body: resolve(__dirname, 'tag/body.html'),
+};
 /** @type {import('vite').UserConfig} */
 export default {
   build: {
     rollupOptions: {
-      input: {
-        main: resolve(__dirname, 'index.html'),
-        a: resolve(__dirname, 'tag/a.html'),
-        p: resolve(__dirname, 'tag/p.html'),
-        div: resolve(__dirname, 'tag/div.html'),
-        marquee: resolve(__dirname, 'tag/marquee.html'),
-        html: resolve(__dirname, 'tag/html.html'),
-        head: resolve(__dirname, 'tag/head.html'),
-      },
+      input: paths,
     },
     // ...
   },
